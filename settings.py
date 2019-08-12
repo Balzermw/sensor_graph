1bc3fdc2fb7497ae67124a4838057e6d4cb9f466
Balzermw <41026897+Balzermw@users.noreply.github.com>
2019-08-11 19:10:30 -0700

    Update settings.py
    formatting

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 class Constants():
-    group_id = 7073
-    access_token = "zvLkNCFVKUwXkEy4VmJnm2TZS2gUbP"
-    door_sensor_id = 212014918418059
-    temp_widget_id = 212014918280284
+    # Group id, ex 4351
+    group_id = 
+    # Access token as provided from API page
+    access_token = "ACCESS_TOKEN_HERE"
+    #temp widget ID, taken from URL
+    temp_widget_id = 
     temperature_end_point = "https://api.samsara.com/v1/sensors/temperature"
     api_time_format = "%Y-%m-%dT%H:%M:%SZ"
